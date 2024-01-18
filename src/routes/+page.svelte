c8b73c6a035bec380cbc0340c958e42fdf28bfb1
Nakaya <eniehack@outlook.jp>
2024-01-18 17:49:19 +0900

    feat: recoder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,130 @@
 <script lang="ts">
 	import { onMount } from "svelte";
+    import { type Writable, writable } from "svelte/store";
+    import stopIcon from "bootstrap-icons/icons/stop-fill.svg";
+    import recordIcon from "bootstrap-icons/icons/record-fill.svg";
 
-    let stopBtn;
-    let startBtn;
+    type RecordingState = "recording" | "recorded" | "notstarted" ;
+    const RECORDING_CODEC = "audio/webm; codec=vorbis";
+    const SPACE_KEY = "Space";
+
+    let recordBtn;
     let playerElem: HTMLAudioElement;
+    let downloadElem: HTMLAnchorElement;    
+    let mediaRecorder: Writable<MediaRecorder | undefined> = writable();
+    let chunks: Writable<Blob[]> = writable([]);
+    let blob: Writable<Blob> = writable();
+    let voiceURL: Writable<string> = writable();
+    let recordingState: Writable<RecordingState> = writable("notstarted");
 
-    const startRecording = (mediaRecorder: MediaRecorder) => {
-        mediaRecorder.start()
-    }
-    
-    const stopRecording = (mediaRecorder: MediaRecorder) => {
-        if (mediaRecorder.state === "inactive") {
+    const handleRecordingStop = () => {
+        if ($recordingState === "recording" && typeof $mediaRecorder !== "undefined") {
+            $mediaRecorder.stop();
+            console.log("saved");
+            //$mediaRecorder = undefined;
             return
         }
-        mediaRecorder.stop();
-        console.log("saved")
     }
 
-    let mediaRecorder: MediaRecorder;
-    let chunks: Blob[] = [];
-    let isRecording = false;
+    const handleRecording = async () => {
+        if (typeof $mediaRecorder === "undefined" || $recordingState === "recording") return
+        $mediaRecorder.start()
+    }
+
+    const handleRecordingStartByKey = (e: KeyboardEvent) => {
+        if (e.code !== SPACE_KEY || $recordingState === "recording") return;
+        console.log("keydown")
+        console.log(e)
+        handleRecording();
+    }
+
+    const handleRecordingStopByKey = (e: KeyboardEvent) => {
+        if (e.code !== SPACE_KEY || $recordingState !== "recording") return; 
+        console.log("keyup")
+        console.log(e)
+        handleRecordingStop();
+    }
+
+    const handleRecordingStartByClick = (e: UIEvent) => {
+        console.log("keydown")
+        console.log(e)
+        handleRecording();
+    }
+
+    const handleRecordingStopByClick = (e: UIEvent) => {
+        console.log("keyup")
+        console.log(e)
+        handleRecordingStop();
+    }
 
     onMount(async () => {
         if (!navigator.mediaDevices) {
             window.alert("mediadevices is unavailable")
             return
         }
-        if (!MediaRecorder.isTypeSupported("audio/ogg")) {
-            window.alert("ogg is unsupported")
+        if (!MediaRecorder.isTypeSupported(RECORDING_CODEC)) {
+            window.alert("unsupported mime type")
             return
         }
-        const stream = await navigator.mediaDevices.getUserMedia({audio: true})
-        mediaRecorder = new MediaRecorder(stream, {mimeType: "audio/ogg"});
-        mediaRecorder.ondataavailable = (e) => {
-            chunks.push(e.data);
+        recordingState.subscribe(s => {
+            if (s === "recorded") {
+                playerElem.style.visibility = "visible"
+            } else {
+                playerElem.style.visibility = "hidden"
+            }
+        })
+        const stream = await navigator.mediaDevices.getUserMedia({audio: true});
+        $mediaRecorder = new MediaRecorder(stream, {mimeType: RECORDING_CODEC});
+        $mediaRecorder.ondataavailable = (e) => {
+            $chunks.push(e.data);
         }
-        mediaRecorder.onstart = () => {
-            isRecording = true;
+        $mediaRecorder.onstart = () => {
+            recordingState.set("recording");
+            console.log("start")
         }
-        mediaRecorder.onstop = () => {
-            const blob = new Blob(chunks, {type: "audio/ogg; codecs=vorbis"});
-            chunks = [];
-            isRecording = false;
-            playerElem.src = URL.createObjectURL(blob);
+        $mediaRecorder.onstop = () => {
+            console.log("stoping")
+            $blob = new Blob($chunks, {type: RECORDING_CODEC});
+            console.log($chunks)
+            $chunks = [];
+            recordingState.set("recorded");
+            playerElem.src = URL.createObjectURL($blob);
+            voiceURL.set(URL.createObjectURL($blob));
         }
     });
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:window on:keydown={handleRecordingStartByKey} on:keyup={handleRecordingStopByKey}/>
 
-{#if !isRecording}
-<button id="start-btn" bind:this={startBtn} on:click={e => startRecording(mediaRecorder)}>start</button>
-{:else}
-<button id="stop-btn" bind:this={stopBtn} on:click={e => stopRecording(mediaRecorder)}>stop</button>
-{/if}
+<button id="record-btn" bind:this={recordBtn} on:pointerdown={handleRecordingStartByClick} on:pointerup={handleRecordingStopByClick}>
+    {#if $recordingState === "recording"}
+        <img src={stopIcon} alt="record stop"/>
+        離してstop
+    {:else}
+        <img src={recordIcon} alt="record start"/>
+        spaceキーかタップしてstart       
+    {/if}
+</button>
 <audio id="player" bind:this={playerElem} controls></audio>
+
+{#if $recordingState === "recorded" && $blob !== null}
+    <a href={$voiceURL}>download</a>
+    {$voiceURL}
+{/if}
+
+
+<style>
+    #record-btn {
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 200px;
+        height: 200px;
+        background: red;
+        color: #FFF;
+        text-decoration: none;
+        text-align: center;
+        margin: 10px 0;
+    }
+</style>
