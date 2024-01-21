0f871632fc8fc10732f9b225c38cb46770abd8c3
Nakaya <eniehack@outlook.jp>
2024-01-21 20:54:19 +0900

    feat: user page

---
+++ src/routes/@[user_id]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+    import { IconUser } from "@tabler/icons-svelte";
+	
+	export let data: PageData;
+</script>
+
+<article>
+    <IconUser size={100} />
+    <h1>{data.name}</h1>
+    <ul>
+        {#each data.recordings as recording (recording.id)}
+            <li>
+                <a href="/s/{recording.id}">{recording.title}</a>
+            </li>
+        {/each}
+    </ul>
+</article>
