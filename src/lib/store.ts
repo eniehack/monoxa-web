27ea1a9caafc267a3b0f496cb27bce906fb42577
Nakaya <eniehack@outlook.jp>
2024-02-01 10:29:05 +0900

    fix

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,6 +1,6 @@
 import { writable, type Writable } from 'svelte/store';
 import type { UserInfo } from 'firebase/auth';
 
-type AuthStore = { loggedIn: true; user: UserInfo } | { loggedIn: false; user: undefined };
+type AuthStore = { loggedIn: true; user: UserInfo } | { loggedIn: false; user: null };
 
 export const authStore: Writable<AuthStore> = writable({ loggedIn: false, user: null });
