4d90ea9f396afcab4ca09fbb6d4270bbf185910f
Nakaya <eniehack@outlook.jp>
2024-02-01 10:02:09 +0900

    feat: changed recorder location

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,21 @@
 <script>
-    
 </script>
 
 <header>
-    <a href="/">voicelog</a>
+	<a href="/">voicelog</a>
+	<a href="/signin">signin</a>
 </header>
 <main class="container">
-    <slot />
+	<slot />
 </main>
 <footer>
-    <a href="http://github.com/eniehack/sechack-demo-voicelog">GitHub</a>
+	<a href="http://github.com/eniehack/sechack-demo-voicelog">GitHub</a>
 </footer>
 
 <style lang="postcss">
-    @import "bootstrap/dist/css/bootstrap.min.css";
-    footer {
-        position: fixed;
-        bottom: 0;
-    }
+	@import 'bootstrap/dist/css/bootstrap.min.css';
+	footer {
+		position: fixed;
+		bottom: 0;
+	}
 </style>
