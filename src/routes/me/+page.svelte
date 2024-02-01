78003a0e7cd639ff3325f199fd76580e2c9a7322
Nakaya <eniehack@outlook.jp>
2024-02-01 10:02:57 +0900

    format

--- src/routes/me/+page.svelte
+++ src/routes/me/+page.svelte
@@ -1,43 +1,49 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-    import { IconUser } from "@tabler/icons-svelte";
-    import { writable, type Writable } from "svelte/store";
-    import Fuse from "fuse.js";
+	import { IconUser } from '@tabler/icons-svelte';
+	import { writable, type Writable } from 'svelte/store';
+	import Fuse from 'fuse.js';
 	import { onMount } from 'svelte';
-	
-	export let data: PageData;
-    let query: Writable<string> = writable("");
-    let recordings = writable(data.recordings);
 
-    onMount(() => {
-        const fuse = new Fuse($recordings, {
-            keys: ["title", "script"],
-        })
-        query.subscribe(val => {
-            if (val.length < 1) {
-                recordings.set(data.recordings);
-                return;
-            }
-            const res = fuse.search(val);
-            console.log(res)
-            $recordings = res.map(rec => rec.item);
-        });
-    });
+	export let data: PageData;
+	let query: Writable<string> = writable('');
+	let recordings = writable(data.recordings);
+
+	onMount(() => {
+		const fuse = new Fuse($recordings, {
+			keys: ['title', 'script']
+		});
+		query.subscribe((val) => {
+			if (val.length < 1) {
+				recordings.set(data.recordings);
+				return;
+			}
+			const res = fuse.search(val);
+			console.log(res);
+			$recordings = res.map((rec) => rec.item);
+		});
+	});
 </script>
 
 <article>
-    <input class="form-control" type="text" name="" bind:value={$query} placeholder="入力してスクリプトを検索">
-    <ul>
-        {#each $recordings as recording (recording.id)}
-            <li>
-                <a href="/s/{recording.id}">{recording.title}</a>
-            </li>
-        {/each}
-    </ul>
+	<input
+		class="form-control"
+		type="text"
+		name=""
+		bind:value={$query}
+		placeholder="入力してスクリプトを検索"
+	/>
+	<ul>
+		{#each $recordings as recording (recording.id)}
+			<li>
+				<a href="/s/{recording.id}">{recording.title}</a>
+			</li>
+		{/each}
+	</ul>
 </article>
 
 <style>
-    /*
+	/*
     #content {
         position: absolute;
         left: 50%;
--- src/routes/s/[shout_id]/+page.svelte
+++ src/routes/s/[shout_id]/+page.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-    import { IconUserFilled, IconScript } from "@tabler/icons-svelte";
-	
+	import { IconUserFilled, IconScript } from '@tabler/icons-svelte';
+
 	export let data: PageData;
 </script>
 
 <div id="content">
-    <h1>{data.title}</h1>
-    <audio src={data.file} controls></audio>
-    <div class="flex items-center">
-        <IconUserFilled size={18}/>
-        <a href={`/@${data.user.id}`}>{data.user.name}</a>
-    </div>
-    <p id="script">{data.script}</p>
+	<h1>{data.title}</h1>
+	<audio src={data.file} controls></audio>
+	<div class="flex items-center">
+		<IconUserFilled size={18} />
+		<a href={`/@${data.user.id}`}>{data.user.name}</a>
+	</div>
+	<p id="script">{data.script}</p>
 </div>
 
 <style>
-    /*
+	/*
     #content {
         position: absolute;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
     } */
-    #script {
-        height: 500px;
-        overflow-y: scroll;
-    }
+	#script {
+		height: 500px;
+		overflow-y: scroll;
+	}
 </style>
